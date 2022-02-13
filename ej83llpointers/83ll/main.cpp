5fc98ea44d6ba5b0649c20c5d75d3ac930392caa
General Krypton <ayrton.arc@gmail.com>
2022-02-12 23:14:54 -0800

    Pointer exercise 2 memory positions en c++

--- ej83llpointers/83ll/main.cpp
+++ ej83llpointers/83ll/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int numero, *dir_numero;
+
+    cout<<"Digite un numero: "; cin>>numero;
+
+    dir_numero =&numero; //
+
+    if(*dir_numero%2==0){
+        cout<<"El num es par "<<*dir_numero <<"es par"<<endl;
+        cout<<"Posision: "<<dir_numero<<endl;
+    }
+    else{
+        cout<<"El numero"<<*dir_numero<<"es impar"<<endl;
+        cout<<"Posision: "<<dir_numero<<endl;
+    }
+    cout << "" << endl;
     return 0;
 }
